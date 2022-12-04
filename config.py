f11095e9a9d793899b9c0e3b2ea7ab50f6b9a6f1
buyahhh <119806866+buyahhh@users.noreply.github.com>
2022-12-05 00:54:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5917720957:AAGiYH_A7IabgHNpyMYsTHB3pdk0XZE6ta8")
     
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 10682533))
     
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH"41a93e59d8940968ba71e92a978e2fcb)
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -24,7 +24,7 @@ class Config(object):
     
     PROCESS_MAX_TIMEOUT = 3600
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "2027520982"))
 
     SESSION_NAME = "UploadLinkToFileBot"
     
